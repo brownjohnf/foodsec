78a0e06e9bb7db5d57b4e8698b4826b36a39c6d0
Jack Brown <jack@brownjohnf.com>
2012-05-24 16:49:40 +0000

    added default admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create(
+  :email => 'admin@example.com',
+  :password => 'password'
+)
+admin.confirm!
